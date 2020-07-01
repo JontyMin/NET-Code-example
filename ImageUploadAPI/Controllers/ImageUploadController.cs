269c0b130316fb940abe413b9a05f702057e107c
JontyWang <501211312@qq.com>
2020-07-01 22:29:08 +0800

    添加项目文件。

---
+++ ImageUploadAPI/Controllers/ImageUploadController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageUploadAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageUploadController : ControllerBase
+    {
+        public static IWebHostEnvironment _environment;
+        public ImageUploadController(IWebHostEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        public class FileUploadAPI
+        {
+            public IFormFile files { get; set; }
+        }
+        [HttpPost]
+        public async Task<string> Post(FileUploadAPI objFile)
+        {
+            try
+            {
+                if (objFile.files.Length > 0)
+                {
+                    if (!Directory.Exists(_environment.WebRootPath + "\\Upload\\"))
+                    {
+                        Directory.CreateDirectory(_environment.WebRootPath + "\\Upload\\");
+                    }
+
+                    using (FileStream fileStream = System.IO.File.Create(_environment.WebRootPath + "\\Upload\\" + objFile.files.FileName))
+                    {
+                        objFile.files.CopyTo(fileStream);
+                        fileStream.Flush();
+                        return "\\Upload\\" + objFile.files.FileName;
+                    }
+                }
+                else
+                {
+                    return "Failed";
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return ex.Message.ToString();
+            }
+        }
+
+    }
+}
