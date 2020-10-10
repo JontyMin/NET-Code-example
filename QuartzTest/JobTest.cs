5b0e5229847c65c5c2cc17e1de05a31846605300
JontyWang <501211312@qq.com>
2020-10-10 23:23:42 +0800

    :checkered_flag:

---
+++ QuartzTest/JobTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace QuartzTest
+{
+    public class JobTest:IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QuartzTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace QuartzTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // todo 挖个坑
+        }
+    }
+}
